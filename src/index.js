97c9c5fc2531639887d8a2be3b1512a16c4dcef7
Javier Antunez <javierantunezg@gmail.com>
2022-04-12 21:49:12 -0500

    archivo navbar creado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Letter from './components/Letter';
+// import Letter from './components/Letter';
+
 import './index.css';
-// import App from './App';
+import App from './App';
 
 ReactDOM.render(
-	<Letter />,
+	<App />,
 	document.getElementById('root'),
 );
