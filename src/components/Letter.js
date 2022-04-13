47a454f00331ea6ffc4597b3552cb35c8bdf5c50
Alejandro Partida <alex.9939@hotmail.com>
2022-04-12 21:33:30 -0500

    change eslint rule for function components

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../styles/letter.css';
 
-function Letter() {
+const Letter = () => {
   return (
 	<div className="grid-input flex h-screen justify-center items-center">
 		<input className=" border-4 border-black border-color: rgb(0, 0, 0)" type="text" maxLength="1" />
 	</div>
   );
-}
+};
 
 export default Letter;
